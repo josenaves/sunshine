04a5f0ca1f4344e98eac5af3a6a4e4fe21214f4a
José Naves <josenaves@gmail.com>
2014-08-14 16:27:53 -0300

    New menu

--- app/src/main/java/com/josenaves/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/josenaves/sunshine/app/ForecastFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,6 +27,12 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecastfragment, menu);
     }
 
     @Override
--- app/src/main/java/com/josenaves/sunshine/app/MainActivity.java
+++ app/src/main/java/com/josenaves/sunshine/app/MainActivity.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
