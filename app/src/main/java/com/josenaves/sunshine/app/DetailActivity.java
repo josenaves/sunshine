c86756b1de19d0a3dd71117a4c9e151b30cf0ea9
José Naves <josenaves@gmail.com>
2014-08-21 17:10:45 -0300

    Share button included in detail activity

--- app/src/main/java/com/josenaves/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/josenaves/sunshine/app/DetailActivity.java
@@ -2,10 +2,13 @@ package com.josenaves.sunshine.app;
 
 import android.content.Intent;
 import android.media.audiofx.BassBoost;
+import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +18,12 @@ import android.os.Build;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
+
+    public static final String SUNSHINE_APP = " #SunshineApp";
+
+    private ShareActionProvider shareActionProvider;
+
+    private static String forecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,24 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+
+        // locate menu item with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // fetch and store ShareActionProvider
+        shareActionProvider = (ShareActionProvider)MenuItemCompat.getActionProvider(item);
+
+        // defines a share intent
+        shareActionProvider.setShareIntent(createShareIntent());
+
         return true;
+    }
+
+    private Intent createShareIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, forecast + SUNSHINE_APP);
+        return intent;
     }
 
     @Override
@@ -50,6 +76,13 @@ public class DetailActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // call to update the share intent
+    private void setShareActionProvider(Intent intent) {
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(intent);
+        }
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -65,7 +98,7 @@ public class DetailActivity extends ActionBarActivity {
             // get data passed by previous activity
             Intent intent = getActivity().getIntent();
             if (null != intent && intent.hasExtra(Intent.EXTRA_TEXT))  {
-                String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
                 TextView detail = (TextView) rootView.findViewById(R.id.text_detail);
                 detail.setText(forecast);
             }
